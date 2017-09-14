27ade9003f1f86cac637c5cda47398c472f8eb02
Lúcás Meier <cronokirby@gmail.com>
2017-09-14 22:02:04 +0200

    documents the card class

--- src/com/cronokirby/flashki/models/Card.kt
+++ src/com/cronokirby/flashki/models/Card.kt
@@ -1,8 +1,23 @@
 package com.cronokirby.flashki.models
 
+/**
+ * Represents a single flashcard
+ *
+ * @param front the front side of the card
+ * @param back the back side of the card
+ */
 class Card(val front: String, val back: String) {
     val id = front + back
 
+    /**
+     * Returns whether or not this card is equivalent to another
+     *
+     * Returns true if the card's id is the same. The id is simply
+     * the concatenation of the front and the back atm, but this may change
+     * in the future.
+     *
+     * @param that the card to compare to
+     */
     fun isThat(that: Card): Boolean {
         return this.id == that.id
     }
